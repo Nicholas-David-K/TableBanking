fef539fd33a1c24f201e64340ac1195514215e79
Nicholas David <onedavid.nicholas@gmail.com>
2020-11-13 15:03:08 +0300

    Updated Settings

--- TableBanking/settings.py
+++ TableBanking/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jt8ys!qvd45asplh%(m@_k(f+^)cw6p-dtv=5b9^zo70uok(nj'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG_VALUE"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['table-banking.herokuapp.com']
 
 
 # Application definition
